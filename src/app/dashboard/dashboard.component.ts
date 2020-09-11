6ec02ecfa779210806f77e67a0b99b675d550c0e
kumararvind213 <kumararvind213@gmail.com>
2020-09-11 11:58:04 +0530

    filter section added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,7 +9,10 @@ import * as $ from 'jquery';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  devp_name= "Arvind kumar Gupta"
   filteredItems : any;
+  launch_success = false;
+  land_success = false;
   url = 'https://api.spacexdata.com/v3/launches?limit=100';
   filterUrl = 'https://api.spaceXdata.com/v3/launches?limit=100&launch_year=';
   years = [2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017];
@@ -27,17 +30,28 @@ export class DashboardComponent implements OnInit {
     )
   }
   filter_method(year){
-     this.filter(year).subscribe(
+     this.filterByYear(year).subscribe(
        result =>{
         this.filteredItems = result;
         console.log(result);
        }
      )
   }
+  filter_all(val1,val2){
+    this.filterRecord(val1, val2).subscribe(
+      result =>{
+           this.filteredItems = result;
+           console.log(result);
+      }
+    )
+  }
   alluserService() {
     return this.http.get<any[]>(this.url).pipe(map(data => data));
   }
-  filter(year){
+  filterByYear(year){
      return this.http.get<any[]>(this.filterUrl + year).pipe(map(data => data));
+  }
+  filterRecord(launch_success,land_success){
+     return this.http.get<any[]>(this.url + '&launch_success=' + launch_success + '&land_success=' + land_success).pipe(map(data => data));
   }
 }
